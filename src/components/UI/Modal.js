876c29834d2eb8d058d1f880ee9872aa6342a3c0
coding-guy <1codingguy@gmail.com>
2022-04-17 17:51:12 +0100

    finished Cart/Modal state functionalities

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,9 @@ import React from 'react'
 import classes from './Modal.module.css'
 import ReactDOM from 'react-dom'
 
-const Backdrop = props => <div className={classes.backdrop} />
+const Backdrop = ({ onClose }) => (
+  <div className={classes.backdrop} onClick={onClose} />
+)
 
 const ModalOverlay = props => {
   return (
@@ -17,7 +19,7 @@ const portalElement = document.getElementById('overlays')
 const Modal = props => {
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
